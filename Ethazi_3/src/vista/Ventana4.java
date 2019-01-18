41814988a19419ae521d2ea590ca03f4cc69d447
Oier <oier.vicentefi@elorrieta-errekamari.com>
2019-01-18 10:04:28 +0100

    Ventana 4

--- Ethazi_3/src/vista/Ventana4.java
+++ Ethazi_3/src/vista/Ventana4.java
@@ -7,41 +7,43 @@ import javax.swing.JTextPane;
 
 public class Ventana4 extends JPanel {
 
+	JLabel lblNewLabel = new JLabel("Aukeratutako Bidaia :");
+	JLabel lblBidaia = new JLabel("Bidaia :");
+	JLabel lblData = new JLabel("Data :");
+	JLabel lblPrezioa = new JLabel("Prezioa :");
+	JTextPane textPaneBidaia = new JTextPane();
+	JTextPane textPaneData = new JTextPane();
+	JTextPane textPanePrezioa = new JTextPane();
+	
+	
 	/**
 	 * Create the panel.
 	 */
 	public Ventana4() {
 		setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Aukeratutako Bidaia :");
 		lblNewLabel.setBounds(83, 24, 297, 34);
 		lblNewLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 24));
 		add(lblNewLabel);
 		
-		JLabel lblBidaia = new JLabel("Bidaia :");
 		lblBidaia.setBounds(38, 80, 83, 26);
 		lblBidaia.setFont(new Font("Bookman Old Style", Font.PLAIN, 20));
 		add(lblBidaia);
 		
-		JLabel lblData = new JLabel("Data :");
 		lblData.setBounds(38, 135, 74, 26);
 		lblData.setFont(new Font("Bookman Old Style", Font.PLAIN, 20));
 		add(lblData);
 		
-		JLabel lblPrezioa = new JLabel("Prezioa :");
 		lblPrezioa.setBounds(38, 188, 91, 34);
 		lblPrezioa.setFont(new Font("Bookman Old Style", Font.PLAIN, 20));
 		add(lblPrezioa);
 		
-		JTextPane textPaneBidaia = new JTextPane();
 		textPaneBidaia.setBounds(139, 80, 104, 20);
 		add(textPaneBidaia);
 		
-		JTextPane textPaneData = new JTextPane();
 		textPaneData.setBounds(139, 135, 104, 20);
 		add(textPaneData);
 		
-		JTextPane textPanePrezioa = new JTextPane();
 		textPanePrezioa.setBounds(139, 202, 104, 20);
 		add(textPanePrezioa);
 
