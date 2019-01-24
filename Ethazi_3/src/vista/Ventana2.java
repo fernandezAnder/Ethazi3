c53dc62e5f140587f3733d232d5a65f14c279ca0
Oier <oier.vicentefi@elorrieta-errekamari.com>
2019-01-24 12:31:29 +0100

    Aldaketa ventana2

--- Ethazi_3/src/vista/Ventana2.java
+++ Ethazi_3/src/vista/Ventana2.java
@@ -1,5 +1,8 @@
 package vista;
 import javax.swing.JLabel;
+
+import controlador.Metodoak;
+
 import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -7,6 +10,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.BorderLayout;
 
 public class Ventana2 extends JFrame {
 
@@ -21,61 +25,65 @@ public class Ventana2 extends JFrame {
 	
 	public Ventana2() {
 		this.setBounds(275,100,700,600);
+		getContentPane().setLayout(null);
 
 		//TEXTO AUKERATU LINEA
 		textoAukeratuLinea.setFont(new Font("Comic Sans MS", Font.BOLD, 24));
 		textoAukeratuLinea.setBounds(265, 43, 193, 35);
-		add(textoAukeratuLinea);
+		getContentPane().add(textoAukeratuLinea);
 
 		//BILBO-MUNGIA BOTOIA
 		btnBilbomungia.setFont(new Font("Bookman Old Style", Font.PLAIN, 20));
 		btnBilbomungia.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				
-				
+	   			 Metodoak.hirugarrenLeihoa();
+	   			 
+	   			 
 			}
 		});
 		btnBilbomungia.setBounds(282, 139, 198, 48);
-		add(btnBilbomungia);
+		getContentPane().add(btnBilbomungia);
 
 		//BILBO-GERNIKA BOTOIA
 		btnBilbogernika.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+	   			 Metodoak.hirugarrenLeihoa();
+
 
 			}
 		});
 		btnBilbogernika.setFont(new Font("Bookman Old Style", Font.PLAIN, 20));
 		btnBilbogernika.setBounds(282, 236, 198, 48);
-		add(btnBilbogernika);
+		getContentPane().add(btnBilbogernika);
 		
 		//BILBO-DONOSTIA BOTOIA
 		btnBilbodonosti.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-				
+	   			 Metodoak.hirugarrenLeihoa();
+
 	
 			}
 		});
 
 		
 		btnBilbodonosti.setFont(new Font("Bookman Old Style", Font.PLAIN, 20));
-		btnBilbodonosti.setBounds(282, 323, 198, 48);
-		add(btnBilbodonosti);
+		btnBilbodonosti.setBounds(282, 324, 198, 48);
+		getContentPane().add(btnBilbodonosti);
 		
 		//ATZERA BOTOIA
 		btnAtzera.setFont(new Font("Bookman Old Style", Font.PLAIN, 16));
 		btnAtzera.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
-		
+	   			 Metodoak.bigarrenLeihoa();
+	   			 
 			}
 		});
-		btnAtzera.setBounds(497, 464, 120, 40);
-		add(btnAtzera);
+		btnAtzera.setBounds(515, 492, 120, 35);
+		getContentPane().add(btnAtzera);
 
 	}
 }
