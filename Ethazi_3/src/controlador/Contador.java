3fa53472a2f2c32908ff304c37f6a52afe11de55
ander <ander.fernandezla@elorrieta-errekamari.com>
2019-02-07 09:17:41 +0100

    aldaketak

--- Ethazi_3/src/controlador/Contador.java
+++ Ethazi_3/src/controlador/Contador.java
@@ -4,21 +4,24 @@ import vista.Ventana7;
 
 public class Contador extends Thread {
    
+	
 	@Override
     public void run(){
-		
+		Ventana7 ventana7= new Ventana7();
+		ventana7.setVisible(true);
     	for (int i=0;i<=5;i++) {
         	try {
 				Thread.sleep(1000);
-				
+
 			}catch(InterruptedException e){
 				e.printStackTrace();
 			}	
     	}
-    	
+    	ventana7.dispose();
     	Metodoak.lehenengoLeihoa();
     }
     	
+	
 }
 
 
