5875d24968e2dd020d3f17481ee8e73535a26f04
ander <ander.fernandezla@elorrieta-errekamari.com>
2019-02-13 09:31:53 +0100

    MetodoakTest

---
+++ Ethazi_3/src/controlador/MetodoakTest.java
@@ -0,0 +1,53 @@
+package controlador;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+
+class MetodoakTest {
+
+	@Test
+	public void metodoakKoprobatu() {
+
+	}
+	public void konprobatuMD5() {
+		
+	}
+	
+	public void  konprobatuDistanciaCoord() {
+		
+	}
+	public void prezioaKalkulatu() {
+		
+	}
+	public void konprobatuNegatibo() {
+		
+	}
+	public void kanbioMetodoa() {
+		
+	}
+	public void konprobatuLetra() {
+		
+	}
+	public void diruFalta() {
+		
+	}
+	public void orduaAtera() {
+		
+	}
+	public void dataAtera() {
+		
+	}
+	public void Redondear() {
+		
+	}
+	public void KalkulatuLetra() {
+		
+	}
+}
